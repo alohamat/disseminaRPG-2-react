3dec51425165a8ab38589b094048e5ca47456551
aloha <matalohazk@gmail.com>
2025-10-16 22:28:00 -0300

    mestres podem criar e personalizar votações, jogadores podem ver e votar, e mestre vê

--- frontend-react/src/pages/VotingPage.tsx
+++ frontend-react/src/pages/VotingPage.tsx
@@ -1,23 +1,65 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { useParams } from "react-router-dom";
-import { Cria_Votacao } from "../components/Dados";
-import { Espera_Votacao } from "../components/Dados";
-import { Votacao_Estado } from "../components/Dados";
+import { Cria_Votacao, Espera_Votacao, Votacao_Estado } from "../components/Dados";
+import { useState } from "react";
 
-export function VotingPage(){
-    const { id } = useParams();
+export function VotingPage() {
+  const { id } = useParams();
+  const [opcoes, setOpcoes] = useState<string[]>([]);
 
-    return(
-        <div id="tudo">
-            <Header />
-            <h1>Criar Votação - Jogador {id}</h1>
-            <section>
-                <div onClick={() => id == undefined ? null: Cria_Votacao(id)}>Criar votacao</div>
-                <div onClick={() => id == undefined ? null: Espera_Votacao(id, ["teste1", "teste2"])}>Criar opcao</div>
-                <div onClick={() => id == undefined ? null: Votacao_Estado(id)}>Votacao estado</div>
-            </section>
-            <Footer />
+  function AdicionarOpcao() {
+    setOpcoes([...opcoes, ""]);
+  }
+
+  function AtualizarOpcao(index: number, valor: string) {
+    const novasOpcoes = [...opcoes];
+    novasOpcoes[index] = valor;
+    setOpcoes(novasOpcoes);
+  }
+
+  return (
+    <div id="tudo">
+      <Header />
+      <h1>Criar Votação - Jogador {id}</h1>
+
+      <section>
+        <div
+          onClick={() => (id === undefined ? null : Cria_Votacao(id))}
+        >
+          Criar votação
         </div>
-    )
+
+        <div onClick={AdicionarOpcao}>Criar opção</div>
+
+        {opcoes.map((valor, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              id={`opcao${index}`}
+              value={valor}
+              onChange={(e) => AtualizarOpcao(index, e.target.value)}
+              placeholder={`Opção ${index + 1}`}
+            />
+          </div>
+        ))}
+
+        <div
+          onClick={() =>
+            id === undefined ? null : Espera_Votacao(id, opcoes)
+          }
+        >
+          Atualizar opções
+        </div>
+
+        <div
+          onClick={() => (id === undefined ? null : Votacao_Estado(id))}
+        >
+          Votação estado
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
 }
