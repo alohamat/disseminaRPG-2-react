5ed31aa70f8de28cc4a94bd52f27e2f0f7c942fb
Irwing <126827415+Irwing-Dev@users.noreply.github.com>
2025-10-22 17:51:14 -0300

    feat: Update Dados.tsx
    Atualizei a requisicao de criar votacao para a atual funcao da rota no backend

--- frontend-react/src/components/Dados.tsx
+++ frontend-react/src/components/Dados.tsx
@@ -29,9 +29,14 @@ export async function Reseta_dados(playerId: string) {
   }
 }
 
-export async function Cria_Votacao(playerId: string) {
+export async function Cria_Votacao(playerId: string, opcao: string[]) {
+  const data = JSON.stringify({"opcao": opcao})
   try {
-    const res = await api.post(`mestre/jogador${playerId}/criaVotacao`);
+    const res = await api.post(`mestre/jogador${playerId}/criaVotacao`, data, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
     console.log("Criei uma votação!", res.data)
   } catch (err: any) {
     console.error("Erro ao criar votacao", err.data)
@@ -56,20 +61,20 @@ export async function Tranca_dados(playerId: string, bonus: number) {
   }
 }
 
-export async function Espera_Votacao(playerId: string, opcao: string[]) {
-  const data = JSON.stringify({"opcao": opcao})
-  console.log("mandando: ", data)
-  try {
-    const res = await api.post(`mestre/jogador${playerId}/esperaVotacao`, data, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    console.log("Mandei opcao: ", res.data)
-  } catch (err: any) {
-    console.error("deu ruim ao mandar opcao: ", err.data)
-  }
-}
+// export async function Espera_Votacao(playerId: string, opcao: string[]) {
+//   const data = JSON.stringify({"opcao": opcao})
+//   console.log("mandando: ", data)
+//   try {
+//     const res = await api.post(`mestre/jogador${playerId}/esperaVotacao`, data, {
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     })
+//     console.log("Mandei opcao: ", res.data)
+//   } catch (err: any) {
+//     console.error("deu ruim ao mandar opcao: ", err.data)
+//   }
+// }
 
 export async function Votacao_Estado(playerId: string) {
   try {
